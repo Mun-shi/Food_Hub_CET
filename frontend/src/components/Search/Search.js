154c7c04839e0196b513cca7c9d986591704a2b5
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2024-06-27 16:18:13 +0530

    update

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -5,7 +5,7 @@ import classes from './search.module.css';
 Search.defaultProps = {
   searchRoute: '/search/',
   defaultRoute: '/',
-  placeholder: 'Search Food Mine!',
+  placeholder: 'Search Food Hub CET!',
 };
 
 export default function Search({
